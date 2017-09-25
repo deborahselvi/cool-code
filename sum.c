9c9405378d1d1f2ca1746cfac751fd02966c1c29
deborahselvi <32254552+deborahselvi@users.noreply.github.com>
2017-09-26 00:07:24 +0530

    Add files via upload

---
+++ sum.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int n,i,sum=0;
+printf("Enter the value of N");
+scanf("%d",n);
+for(i=0;i<n;i++)
+sum+=i;
+printf("The sum of N numbers is %d",sum);
+}
